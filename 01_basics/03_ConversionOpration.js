ebfe0a33fdaa11103020a2786780e42c301a9b70
tushaljadhav <167062306+tushaljadhav@users.noreply.github.com>
2025-07-30 10:50:54 +0000

    conversionopration

---
+++ 01_basics/03_ConversionOpration.js
@@ -0,0 +1,33 @@
+let score = "35"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueIntnumber = Number(score)
+// console.log(valueIntnumber);
+// console.log(typeof valueIntnumber);
+
+
+
+
+let value_1 = "31"
+let convert_value_1 = Number (value_1)
+console.log(convert_value_1,typeof (convert_value_1));
+// "31" => 31 string convert into number
+
+
+
+let value_2 = "31abc"
+let convert_value_2 = Number (value_2)
+console.log(convert_value_2,typeof (convert_value_2));
+// "31abc" => NaN , in string adding some leters also then it is not covert into number but  it typeof show number.
+
+
+let value_3 = 1
+let convert_value_3 = Boolean (value_3)
+console.log(convert_value_3,typeof (convert_value_3));
+//  true => 1; false => 0; , boolean covert into number  
+// "" => false 
+// "tushal" =>
+
+
