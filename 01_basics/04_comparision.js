4f350765df3e26975ae112978406404e7650df08
tushaljadhav <167062306+tushaljadhav@users.noreply.github.com>
2025-08-01 14:53:04 +0000

    04_comparision.js is add

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+//Comparision Operators 
+
+// console.log( 2 > 1 );   //2 is greater than 1
+// console.log( 2 >= 1 );  //2 is greater or equal to 1
+// console.log( 2 < 1 );   //2 is not less than 1
+// console.log( 2 == 1 );  //2 is not equal to 1
+// console.log( 2 != 1 );  //2 is not equal to 1
+
+
+console.log(null > 0);  //null becomes 0, so 0 > 0 is false
+console.log(null == 0); //null only equals undefined, not 0
+console.log(null >= 0); //null becomes 0, so 0 >= 0 is true
+
+
+console.log(undefined > 0);  //undefined becomes NaN → always false
+console.log(undefined == 0); //undefined only equals null
+console.log(undefined >= 0); //NaN >= 0 is always false
+
+
+
+console.log("2" == 2); //here is == this opereter is coverting string into number and compare
+
+
+// === this is strick checking 
+console.log("2" === 2); //"===" checks for both value and type equality without type conversion.
+
+
+
