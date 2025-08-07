186f84eabfaa8a2a2691c581299a2145fedd1595
tushaljadhav <167062306+tushaljadhav@users.noreply.github.com>
2025-08-07 15:49:53 +0000

    create new file for objects

--- 02_basics/03_Objects.js
+++ 02_basics/03_Objects.js
@@ -51,3 +51,4 @@ jsUser.greetingtwo = function() {
 
 console.log(jsUser.greeting());       // calls first greeting method and logs message
 console.log(jsUser.greetingtwo());    // calls second greeting method with name
+
