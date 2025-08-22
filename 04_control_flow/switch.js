8550fececa653991c5a0adc2e5cb3f4f4746d90f
tushaljadhav <167062306+tushaljadhav@users.noreply.github.com>
2025-08-22 12:31:21 +0000

    create new file "switch.js"

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,74 @@
+// this is the basic code of switch case 
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+const month = 4
+
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    
+    default: 
+    console.log("default case match");
+    
+        break;
+}
+
+const monthInstring = "june"
+switch (monthInstring) {
+    case "jan":
+        console.log("jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    case "may":
+        console.log("may");
+        break;
+    case "june":
+        console.log("june");
+        break;
+    case "july":
+        console.log("july");
+        break;
+    
+    default: 
+    console.log("default case match");
+    
+        break;
+}
+
