4463015d38333fb983774ae841387d76d8dd784d
tushaljadhav <167062306+tushaljadhav@users.noreply.github.com>
2025-08-26 12:08:58 +0000

    create new file "two.js" in 05_iterations folder

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+// while and do-while loops
+
+
+
+let index = 0
+while (index <= 10) {
+    // console.log(`index is ${index}`);
+    index = index + 2
+}
+
+//array 
+let myarry = ["one", "two", "three"]
+let arr = 0
+while (arr < myarry.length) {
+    // console.log(`array is ${myarry[arr]}`);
+    arr = arr + 1
+    
+}
+
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score <= 10);
