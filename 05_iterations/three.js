11d99ae2054d51f7286aa48b1836ab67278558a9
tushaljadhav <167062306+tushaljadhav@users.noreply.github.com>
2025-09-03 12:15:57 +0000

    create new file "three.js"

---
+++ 05_iterations/three.js
@@ -0,0 +1,24 @@
+// for of loop 
+
+["", "", "", ""]
+[ {},{},{},{} ]
+
+
+
+
+arry = [ 1 , 2 , 3 , 4 , 5 , 6 ]
+
+for (const num of arry) {
+    console.log(num);
+    
+    
+}
+
+
+const myarry =  "hello world!" 
+
+for (const my of myarry) {
+    console.log(`each char is ${my}`);
+    
+    
+}
