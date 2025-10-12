888057a0a91856fa38f055822e3c94efe851cb33
tushaljadhav <167062306+tushaljadhav@users.noreply.github.com>
2025-10-12 12:17:01 +0000

    creating new file "six.js" in 05

---
+++ 05_iterations/six.js
@@ -0,0 +1,16 @@
+const coding = ["js", "ruby", "java", "python" , "cpp"]
+
+
+// const values =  coding.forEach ((item) => {
+//     console.log(item);
+//     return item 
+    
+// })
+
+// console.log(values);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+const newNums = myNums.filter((num) => num > 4)
+console.log(newNums);
+
