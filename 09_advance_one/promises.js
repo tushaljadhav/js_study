fc67788bd2c55b72fefbf2bebbaa14186e8c5d88
tushaljadhav <167062306+tushaljadhav@users.noreply.github.com>
2025-11-15 16:17:36 +0000

    create new file "promises.js"

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,26 @@
+const promiseOne = new Promise(function(resolve , reject){
+    // Do an async task 
+    // DB calls , cryptography , network
+    setTimeout(function(){
+        console.log('Async task is  compelete');
+        resolve()
+        
+    },1000)    
+})
+
+
+promiseOne.then(function(){
+    console.log("promise cunsumed");
+    
+})
+
+
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("Async task 2 ");
+        resolve()
+    },1000)
+}).then(function(){
+    console.log("Async 2 resolved");
+    
+})
