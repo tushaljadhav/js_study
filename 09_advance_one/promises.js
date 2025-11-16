f855b88f2d57f0bd9e9e1902f2b1c906e7d7c746
tushaljadhav <167062306+tushaljadhav@users.noreply.github.com>
2025-11-16 12:22:55 +0000

    updated file "promises.js"

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -41,12 +41,71 @@ promisethree.then(function(user){
 
 const promiseFour = new Promise(function(resolve,reject){
     setTimeout(function(){
-        let error = true;
+        let error = true  
         if(!error){
-            resolve({username:"hitesh", password:"123"})
-        }else{
-            reject('ERROR: somthing went wrong')
+                resolve({username: "tushal", password: "123"})
+        }  else{
+            reject('ERROR Somthing went wrong ')
         }
-        
+    },1000)
+}) 
+
+promiseFour.then((user)=>{
+    console.log(user);
+    return user.username
+}).then((username)=>{
+    console.log(username);
+    
+}).catch(function(error){
+    console.log(error);
+    
+}).finally(()=> console.log("the promise is inter resolve or rejectes"))
+
+const promiseFive =  new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true  
+        if(!error){
+                resolve({username: "js", password: "123"})
+        }  else{
+            reject('ERROR: js went wrong ')
+        }
     },1000)
 })
+
+async function cunsumePromiseFive() {
+try {
+    const response = await promiseFive
+    console.log(response);
+} catch (error) {
+    console.log(error);
+        
+}
+    
+}
+
+cunsumePromiseFive()
+
+
+// async function getAllUsers() {
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+        
+//     } catch (error) {
+//         console.log("E:" , error);
+        
+//     }
+// }
+
+// getAllUsers()
+
+fetch('https://api.github.com/users/tushaljadhav')
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+    
+})
+.catch((error)=>console.log(error))
