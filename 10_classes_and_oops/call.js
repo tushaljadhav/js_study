40d84f42da13ce98fa3eeb9d5294103d64dea735
tushaljadhav <167062306+tushaljadhav@users.noreply.github.com>
2025-11-22 12:09:18 +0000

    updated file

--- 10_classes_and_oops/call.js
+++ 10_classes_and_oops/call.js
@@ -1,15 +1,23 @@
+// A simple function to set username on the current object
 function SetUsername(username) {
-    this.username=username
+    this.username = username
     console.log("called");
-    
 }
 
-function createUser(username,email,password) {
-    SetUsername.call(this ,username)
+// Constructor function to create a new user
+function createUser(username, email, password) {
+
+    // Using call() so SetUsername runs in the context of createUser
+    // and 'this' refers to the new user object
+    SetUsername.call(this, username)
+
+    // Adding more properties to the user object
     this.email = email
     this.password = password
 }
 
-const chai = new createUser ("chai","chai@gmail.com","chai123")
+// Creating a new user object using 'new'
+const chai = new createUser("chai", "chai@gmail.com", "chai123")
 
+// Printing the user object
 console.log(chai);
