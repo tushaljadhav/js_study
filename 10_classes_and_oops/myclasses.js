bfa6d315a4bb19f76458ef2490019f474d21edaf
tushaljadhav <167062306+tushaljadhav@users.noreply.github.com>
2025-11-22 13:57:37 +0000

    adding comments

--- 10_classes_and_oops/myclasses.js
+++ 10_classes_and_oops/myclasses.js
@@ -1,17 +1,18 @@
-// // ES6
-
+// ------------------ ES6 CLASS VERSION (Commented Out) ------------------
 // class User {
 //     constructor(username , email , password){
+//         // set properties
 //         this.username = username
 //         this.email = email
 //         this.password = password
-
 //     }
 
+//     // method to encrypt password
 //     encryptPassword(){
 //         return `${this.password}abc`
 //     }
 
+//     // method to change username to uppercase
 //     changeUsername(){
 //         return `${this.username.toUpperCase()}`
 //     }
@@ -23,24 +24,27 @@
 // console.log(chai.changeUsername());
 
 
-// behind the scene
+// ------------------ BEHIND THE SCENES (Using Prototype) ------------------
 
-function User(username,email,password) {
-        this.username = username
-        this.email = email
-        this.password = password
+// Constructor function (old way before classes)
+function User(username, email, password) {
+    this.username = username
+    this.email = email
+    this.password = password
 }
 
+// adding method to prototype → shared by all objects
 User.prototype.encryptPassword = function () {
     return `${this.password}abc`
-    
 }
+
+// another method added to prototype
 User.prototype.changeUsername = function () {
     return `${this.username.toUpperCase()}`
-    
 }
 
-const tea = new User("tea","tea@gmail.com","123")
+// creating object using constructor function
+const tea = new User("tea", "tea@gmail.com", "123")
 
-console.log(tea.encryptPassword());
-console.log(tea.changeUsername());
+console.log(tea.encryptPassword());  // "123abc"
+console.log(tea.changeUsername());   // "TEA"
